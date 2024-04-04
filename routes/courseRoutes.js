cd9e630b9744beca8d2d3e1dcd9784a8362e99b1
Raj Dwivedi <rajdwivedipc@gmail.com>
2024-04-04 12:47:34 +0530

    adding editing of courses

--- routes/courseRoutes.js
+++ routes/courseRoutes.js
@@ -27,6 +27,8 @@ courseRouter.post('/create',verifyToken, async (req, res) => { // WORKS!!
 })
 
 import {addMultipleModules} from "../customModules/cModule.js";
+import mongoClient, {contentDb} from "../dbSetup.js";
+import {ObjectId} from "mongodb";
 
 courseRouter.post('/create/asOne',verifyToken, async (req, res) => {
     const data = req.body.data;
@@ -71,6 +73,67 @@ courseRouter.post('/delete',verifyToken, async (req, res) => {
         console.log('Error in POST Router /delete', e);
         return res.status(500).json({message: 'Internal Server Error'});
     }
+})
+
+
+courseRouter.patch('/update', async (req, res)=> {
+    const courseId = req.body.courseId;
+    console.log(courseId)
+    if(courseId === undefined) {
+        return res.status(400).json({message: "Course ID is missing."});
+    }
+
+    // get course details
+    const courseDetails = await mongoClient.db(contentDb).collection('courses').findOne({_id: new ObjectId(courseId)});
+    console.log(courseDetails)
+    if (courseDetails === null ) {
+        return res.status(404).json({message: "Course not found."});
+    } else if (courseDetails.length === 0) {
+        return res.status(400).json({message: "Course not found."});
+    }
+    else if (req.body.title === undefined &&
+        req.body.description === undefined &&
+        req.body.categories === undefined &&
+        req.body.price === undefined &&
+        req.body.skill_lvl === undefined) {
+        return res.status(400).json({message: "No fields to update."});
+    }
+
+
+    if (req.body.categories || req.body.categoryInsert === undefined) {
+        const warning = "Categories are missing." +
+            " appending them by default. provide 'categoryInsert' field with the value 'replace' to replace.";
+    }
+
+
+    if(req.body.title) {
+        courseDetails.title = req.body.title;
+    }
+    if(req.body.description) {
+        courseDetails.description = req.body.description;
+    }
+    if(req.body.categories) {
+        if (req.body.categoryInsert === 'replace')
+            courseDetails.categories = req.body.categories
+
+        else courseDetails.categories.push(req.body.categories)
+
+    }
+    if(req.body.price) {
+        courseDetails.price = req.body.price;
+    }
+    if(req.body.skill_lvl) {
+        courseDetails.skill_lvl = req.body.skill_lvl;
+    }
+
+    try {
+        const updatedCourse = await mongoClient.db(contentDb).collection('courses').updateOne({_id: new ObjectId(courseId)}, {$set: courseDetails});
+        res.status(200).json(updatedCourse);
+    } catch (e) {
+        console.log('Error in PATCH Router /update', e);
+        return res.status(500).json({message: 'Internal Server Error'});
+    }
+
 })
 
 
